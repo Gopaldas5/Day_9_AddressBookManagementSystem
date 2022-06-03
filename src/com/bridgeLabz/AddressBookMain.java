1482bb4e1ea622ea59cd7df22400d545b050a357
GopalDas <gopalkrdas1997@gmail.com>
2022-06-03 22:19:52 +0530

    ADD : Welcom to the address book management system

---
+++ src/com/bridgeLabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Management System");
+
+    }
+}
