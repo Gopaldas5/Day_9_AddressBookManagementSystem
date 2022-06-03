5416bfce0566d010980c15a81c936704ef1af3ee
GopalDas <gopalkrdas1997@gmail.com>
2022-06-04 00:14:13 +0530

    ADD: Ability to add new contact in Address book

--- src/com/bridgeLabz/AddressBook.java
+++ src/com/bridgeLabz/AddressBook.java
@@ -1,4 +1,34 @@
 package com.bridgeLabz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+//    Contact contact;
+    static Scanner scanner = new Scanner(System.in);
+    ArrayList<Contact> list = new ArrayList();
+
+ public void addNewContact(){
+     System.out.println("Enter the First Name:- ");
+     String firstName = scanner.next();
+     System.out.println("Enter the Last Name:- ");
+     String lastName = scanner.next();
+     System.out.println("Enter the Address :- ");
+     String address = scanner.next();
+     System.out.println("Enter the Name of city:- ");
+     String city = scanner.next();
+     System.out.println("Enter the Name of State:- ");
+     String state = scanner.next();
+     System.out.println("Enter the pin code:- ");
+     String pin = scanner.next();
+     System.out.println("Enter the phone number:- ");
+     String phoneNumber = scanner.next();
+     System.out.println("Enter the Email:-");
+     String email = scanner.next();
+     Contact contact = new Contact(firstName,lastName,address, city, state, pin, phoneNumber, email );
+     list.add(contact);
+
+
+
+ }
 }
--- src/com/bridgeLabz/AddressBookMain.java
+++ src/com/bridgeLabz/AddressBookMain.java
@@ -3,6 +3,8 @@ package com.bridgeLabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Management System");
-
+     AddressBook addressBook = new AddressBook();
+     addressBook.addNewContact();
+        System.out.println(addressBook.list);
     }
 }
